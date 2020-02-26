42733e2bdcf919724976b71083c18c798a26f2ae
sabyrd39 <sbyrd39@uncc.edu>
2020-02-26 16:59:05 -0500

    This is my commit for part 4

--- src/main/java/com/uncc/ticket/TicketApplication.java
+++ src/main/java/com/uncc/ticket/TicketApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import static org.springframework.boot.SpringApplication.run;
+
 @SpringBootApplication
 public class TicketApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TicketApplication.class, args);
+        run(TicketApplication.class, args);
     }
 
 }
